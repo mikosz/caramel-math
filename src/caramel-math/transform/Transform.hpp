56da2a1d86e190f7b6e19966fd67c0e604a8ac4d
Mikołaj Radwan <mikoszrrr@gmail.com>
2018-04-03 18:49:49 +0200

    Beginning work on transforms

---
+++ src/caramel-math/transform/Transform.hpp
@@ -0,0 +1,7 @@
+#ifndef CARAMELMATH_TRANSFORM_TRANSFORM_HPP__
+#define CARAMELMATH_TRANSFORM_TRANSFORM_HPP__
+
+#include "Transform.template.hpp"
+//#include "Transform.implementation.hpp"
+
+#endif /* CARAMELMATH_TRANSFORM_TRANSFORM_HPP__ */
---
+++ src/caramel-math/transform/Transform.template.hpp
@@ -0,0 +1,16 @@
+#ifndef CARAMELMATH_TRANSFORM_TRANSFORM_TEMPLATE_HPP__
+#define CARAMELMATH_TRANSFORM_TRANSFORM_TEMPLATE_HPP__
+
+namespace caramel_math {
+namespace transform {
+
+template <class StorageType>
+class Transform final : StorageType {
+public:
+
+};
+
+} // namespace transform
+} // namespace caramel_math
+
+#endif /* CARAMELMATH_TRANSFORM_TRANSFORM_TEMPLATE_HPP__ */
