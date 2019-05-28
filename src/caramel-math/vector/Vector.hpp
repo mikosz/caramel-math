af1fda8d81c4db98b001e868ccfef9b3c4b5a9ea
Mikołaj Radwan <mikoszrrr@gmail.com>
2019-05-28 21:45:05 +0200

    Beginning work on vector

---
+++ src/caramel-math/vector/Vector.hpp
@@ -0,0 +1,6 @@
+#ifndef CARAMELMATH_VECTOR_VECTOR_HPP__
+#define CARAMELMATH_VECTOR_VECTOR_HPP__
+
+#include "Vector.template.hpp"
+
+#endif /* CARAMELMATH_VECTOR_VECTOR_HPP__ */
---
+++ src/caramel-math/vector/Vector.template.hpp
@@ -0,0 +1,16 @@
+#ifndef CARAMELMATH_VECTOR_VECTOR_TEMPLATE_HPP__
+#define CARAMELMATH_VECTOR_VECTOR_TEMPLATE_HPP__
+
+namespace caramel_math::vector {
+
+template <class StorageType>
+class Vector final : StorageType {
+public:
+
+private:
+
+};
+
+} // namespace caramel_math::vector
+
+#endif /* CARAMELMATH_VECTOR_VECTOR_TEMPLATE_HPP__ */
